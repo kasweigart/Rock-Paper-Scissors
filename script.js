7740d4a729b991b10eb5ec43d310cb1c4f810399
Kyle Sweigart <kasweigart@gmail.com>
2020-03-18 09:33:48 -0400

    manually finished each outcome in js. this is why we use loops!

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-function computerPlay() {
-    let action = ['Rock', 'Paper', 'Scissors'];
-    return Math.floor(Math.random() * action.length);
+function compPlay() {
+    let actions = ['Rock', 'Paper', 'Scissors'];
+    let randAct = actions[Math.floor(Math.random() * actions.length)];
+    return randAct;
+};
+
+
+
+
+
+function playGame(playerSelect, compSelect) {
+    compPlay = compSelect;
+    
+    if (playerSelect == playerSelect.match(/rock/i) && compSelect == "Rock") {
+        return 'Tie! No winner or loser here.';
+    } else if 
+        (playerSelect == playerSelect.match(/paper/i) && compSelect == "Paper") {
+        return 'Tie! No winner or loser here.';
+    } else if (playerSelect == playerSelect.match(/scissors/i) && compSelect == "Scissors") {
+        return 'Tie! No winner or loser here.'; 
+    } else if 
+        (playerSelect == playerSelect.match(/rock/i) && compSelect == "Paper") {
+        return 'Loser! Paper beats rock.';
+    } else if (playerSelect == playerSelect.match(/paper/i) && compSelect == "Scissors") {
+        return 'Loser! Scissors beats paper.'; }
+      else if 
+        (playerSelect == playerSelect.match(/scissors/i) && compSelect == "Rock") {
+        return 'Loser! Rock beats scissors.';
+    } else if (playerSelect == playerSelect.match(/rock/i) && compSelect == "Scissors") {
+        return 'Winner! Rock beats scissors.'; }
+      else if 
+        (playerSelect == playerSelect.match(/paper/i) && compSelect == "Rock") {
+        return 'Winner! Paper beats rock.';
+    } else (playerSelect == playerSelect.match(/scissors/i) && compSelect == "Paper")
+        return 'Winner! Scissors beats paper.'; 
 }
 
-console.log(computerPlay());
+
+const playerSelect = 'rOck';
+const compSelect = compPlay();
+console.log(playGame(playerSelect, compSelect)); 
